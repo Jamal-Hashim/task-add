3d389332f4dc02bfe4ec338f96c316730ba6cdd2
Jamal-Hashim <jamalhahsim9120@gmail.com.com>
2025-09-16 23:15:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// Q1. Write a program that prints numbers from 1 to 20.
+// If the number is divisible by 3 → print "Fizz"
+// If divisible by 5 → print "Buzz"
+// If divisible by both 3 & 5 → print "FizzBuzz"
+// Otherwise, print the number.
+
+// let n = 20;
+// for (let i = 0; i <= n; i++) {
+
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuz");
+//     } else if (i % 3 == 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Function
+
+// function hello() {
+//     console.log("Hello");
+// }
+// hello();
+
+// Function with Parameters
+
+// function greet(name) {
+//     console.log("Name : "+name);
+// }
+// greet("Jamal");
+
+// Function with Return Value
+
+// function add(a,b) {
+//     return a+b;
+// }
+// let result=add(10,20);
+// console.log("Sum : "+result);
+
+// Arrow Functions (short form)
+
+// const sum=(a,b)=>a+b;
+// console.log(sum(10,20));
+
+// Function to Square a Number
+
+// function square(n) {
+//     return n*n;
+// }
+// console.log(square(10));
+// console.log(square(5));
+
+// Function to Greet User
+
+// function greet(name,age) {
+//     console.log("Hello "+name+" you are "+age+" years old");
+// }
+// greet("Jamal",20);
+// greet("Jamal Hashim",22);
+
